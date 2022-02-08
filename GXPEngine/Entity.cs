27af02eb551c002ba2e0b4b2efc777b14149b98d
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-02-08 17:08:23 +0100

    Test thingy, starting off with the given barry.png sprite, animated it randomly and you can just walk around the screen, most important is that Player.cs is based on Entity.cs

---
+++ GXPEngine/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using GXPEngine.Core;
+
+namespace GXPEngine
+{
+    /// <summary>
+    /// Class that covers all types of entities (players and enemies)
+    /// </summary>
+    public class Entity : AnimationSprite
+    {
+        protected Vector2 velocity;
+        protected float speed;
+
+
+        protected Entity(string path, int cols, int rows, int frames =-1) : base(path, cols, rows, frames)
+        {
+            
+        }
+
+        protected virtual void UpdateMovement()
+        {
+            MoveUntilCollision(velocity.x * Time.deltaTime * speed, velocity.y * Time.deltaTime * speed);
+        }
+    }
+}
