afd24f0d61ba4aae4f67e9186b36d7333cb0e12d
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-02-09 14:57:07 +0100

    Debug update

---
+++ GXPEngine/Barrier.cs
@@ -0,0 +1,22 @@
+﻿namespace GXPEngine
+{
+    public class Barrier : Sprite
+    {
+        private EasyDraw canvas;
+
+        Barrier() : base("barrier.png", addCollider: true)
+        {
+            canvas = new EasyDraw(width, height);
+        }
+
+        void Update()
+        {
+            if (debugMode)
+            {
+                canvas.Clear(255,0,0);
+            }
+        }
+        
+        
+    }
+}
